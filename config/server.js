4ea731da9715deaf57682e7898778e36f9a0d0bb
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-12-10 23:34:18 +0700

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '370ca4b48501bf329d7b6aad73f1a2ff'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e909fb7-e9ca-4d94-9302-3cc4de94af2b'
+};
