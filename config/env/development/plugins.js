d3131bc16cd46a476c48644d316477cdf0f681a9
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-12-12 04:53:51 +0700

    fix bad configuration

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -1,13 +1,4 @@
 module.exports = ({ env }) => ({
-  upload: {
-      provider: 'google-cloud-storage',
-      providerOptions: {
-          bucketName: 'storage-vms-bucket',
-          publicFiles: false,
-          uniform: false,
-          basePath: '',
-      },
-  },
   email: {
     provider: 'sendgrid',
     providerOptions: {
