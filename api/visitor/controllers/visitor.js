ed8f3d9e433d7e50c1fc4329d614aa017ce54868
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-12-12 03:30:56 +0700

    send email visitor

--- api/visitor/controllers/visitor.js
+++ api/visitor/controllers/visitor.js
@@ -1,8 +1,35 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.visitor.create(data, { files });
+    } else {
+      entity = await strapi.services.visitor.create(ctx.request.body);
+    }
+
+    entry = sanitizeEntity(entity, { model: strapi.models.visitor });
+
+    const emailUser = ctx.request.body.email || '';
+    // send an email by using the email plugin
+    await strapi.plugins['email'].services.email.send({
+      to: emailUser,
+      from: 'admin@strapi.io',
+      subject: 'Kirim email yes',
+      text: `
+        Berhasil kirim email yap...
+      `,
+    });
+
+
+    return entry;
+  },
+};
