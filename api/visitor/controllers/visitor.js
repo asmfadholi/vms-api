41cf089eb39f31db6a63695f9964a589e67f05aa
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-12-13 13:39:21 +0700

    fix: revis template email

--- api/visitor/controllers/visitor.js
+++ api/visitor/controllers/visitor.js
@@ -1,6 +1,6 @@
 'use strict';
 const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-
+const templateEmailView = require('../../../template/htmlVisitor');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -18,15 +18,15 @@ module.exports = {
 
     const entry = sanitizeEntity(entity, { model: strapi.models.visitor });
 
+    const { name = '', area = {}, email = '' } = entry;
+
     const emailUser = ctx.request.body.email || '';
     // send an email by using the email plugin
     await strapi.plugins['email'].services.email.send({
       to: emailUser,
       from: 'mochammad.fadholi.st@gmail.com',
-      subject: 'Kirim email yes',
-      text: `
-        Berhasil kirim email yap...
-      `,
+      subject: `SELAMAT kamu berhasil terdaftar di area ${area.name || 'ini'}`,
+      html: templateEmailView({ name, productName: area.name || '', email })
     });
 
 
