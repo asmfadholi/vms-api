8bbb7df357bcc29958182e8426aea19103993021
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-12-29 23:33:56 +0700

    chore: setup documentation

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,11 @@
 module.exports = ({ env }) => ({
   upload: {
-      provider: 'google-cloud-storage',
-      providerOptions: {
-          bucketName: env('BUCKET_NAME'),
-          publicFiles: false,
-          uniform: false,
-          basePath: '',
-      },
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
   },
   email: {
     provider: 'sendgrid',
