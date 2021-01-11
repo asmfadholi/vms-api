6888c16088187c13ca9f1ea8bc86e306280ab9e0
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-01-11 18:58:46 +0700

    fix: un comment sendgrid

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,14 +8,14 @@ module.exports = ({ env }) => ({
           basePath: '',
       },
   },
-  // email: {
-  //   provider: 'sendgrid',
-  //   providerOptions: {
-  //     apiKey: env('SENDGRID_API_KEY'),
-  //   },
-  //   settings: {
-  //     defaultFrom: 'mochammad.fadholi.st@gmail.com',
-  //     defaultReplyTo: 'mochammad.fadholi.st@gmail.com',
-  //   },
-  // },
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'mochammad.fadholi.st@gmail.com',
+      defaultReplyTo: 'mochammad.fadholi.st@gmail.com',
+    },
+  },
 });
