a3d00d85024259a0e9868fcaa714bc480110bf56
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-02-02 12:05:29 +0700

    fix: revise from email

--- api/visitor/controllers/visitor.js
+++ api/visitor/controllers/visitor.js
@@ -39,7 +39,7 @@ module.exports = {
     // send an email by using the email plugin
     await strapi.plugins['email'].services.email.send({
       to: emailUser,
-      from: 'mochammad.fadholi.st@gmail.com',
+      from: 'no-reply@outing.id',
       subject: `SELAMAT kamu berhasil terdaftar di area ${area.name || 'ini'}`,
       html: templateEmailView({ name, productName: area.name || '', email })
     });
