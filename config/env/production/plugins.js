b1df6de55ad1eccf502ed082e3e64276d611a52a
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-04-14 05:14:17 +0700

    change email sender

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,14 +8,27 @@ module.exports = ({ env }) => ({
           basePath: '',
       },
   },
+  // email: {
+  //   provider: 'sendgrid',
+  //   providerOptions: {
+  //     apiKey: env('SENDGRID_API_KEY'),
+  //   },
+  //   settings: {
+  //     defaultFrom: 'no-reply@outing.id',
+  //     defaultReplyTo: 'no-reply@outing.id',
+  //   },
+  // },
   email: {
-    provider: 'sendgrid',
+    provider: 'mandrill',
     providerOptions: {
-      apiKey: env('SENDGRID_API_KEY'),
+      mandrill_api_key: env('MANDRILL_API_KEY'),
+      mandrill_default_replyto: 'no-reply@outing.id',
+      mandrill_default_from_email: 'no-reply@outing.id',
     },
-    settings: {
-      defaultFrom: 'no-reply@outing.id',
-      defaultReplyTo: 'no-reply@outing.id',
-    },
+    // settings: {
+    //   defaultFrom: 'no-reply@outing.id',
+    //   defaultReplyTo: 'no-reply@outing.id',
+    // },
   },
+
 });
