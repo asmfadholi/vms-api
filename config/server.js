dbf5ac65c3468fdeddf24c81a6e03ddc1b57ad25
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-06-03 19:15:35 +0700

    chore: setup ui

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: env('API_URL', '/'),
+  url: env('API_URL', '/'),
   admin: {
-    // url: '/',
+    url: '/',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '370ca4b48501bf329d7b6aad73f1a2ff'),
     },
